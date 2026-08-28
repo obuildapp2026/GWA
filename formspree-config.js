5ded1b90381e3aeb5dcfcbb586518bae0bb427de
ObrantoApp <obuild.app2026@gmail.com>
2026-08-28 17:29:09 +0200

    Update formspree-config.js

--- formspree-config.js
+++ formspree-config.js
@@ -1,16 +1,3 @@
-/*
-  GWA QuickScan - Formspree configuration
-
-  Replace ONLY the value below with the endpoint shown in:
-  Formspree -> GWA QuickScan -> Settings
-
-  Example:
-  https://formspree.io/f/xxxxxxxx
-
-  The current Formspree endpoint was not present in the supplied files,
-  so it is deliberately NOT fabricated here.
-*/
 const FORMSPREE_CONFIG = {
-  const FORMSPREE_CONFIG = {
   endpoint: "https://formspree.io/f/mvkoovev"
 };
